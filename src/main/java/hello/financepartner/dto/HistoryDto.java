9dcdbbd0275da5582ee3393a83785dc10e6cc074
Hyunuk Shin <shu914@naver.com>
2024-06-07 21:56:17 +0900

    [BE] DTO 및 서비스 로직 추가

---
+++ src/main/java/hello/financepartner/dto/HistoryDto.java
@@ -0,0 +1,28 @@
+package hello.financepartner.dto;
+
+import hello.financepartner.domain.status.Category;
+import hello.financepartner.domain.status.IsIncom;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class HistoryDto {
+
+    @Builder
+    @Data
+    @Schema(description = "수입/지출 정보")
+    public static class Info {
+        Long id;
+        LocalDate date;
+        Long amount;
+        String content;
+        Category category;
+        IsIncom isIncome;
+    }
+
+
+}
