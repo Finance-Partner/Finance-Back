1f4f6b9cd3eee70be6840fc48fd1beb4290ca166
Hyunuk Shin <shu914@naver.com>
2024-06-07 21:53:32 +0900

    [BE] 도메인 파일추가 (데이터베이스 테이블)

---
+++ src/main/java/hello/financepartner/domain/status/Category.java
@@ -0,0 +1,6 @@
+package hello.financepartner.domain.status;
+
+public enum Category {
+//월급, 이자, 식사, 쇼핑, 카페 · 간식, 교통, 편의점 · 마트, 기타
+    SALARY, INTEREST, MEAL, SHOPPING, CAFE_SNACK, TRANSPORT, CONVSTORE_MART, ETC
+}
